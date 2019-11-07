4634132230953991d24901b7f17f84ad459be64d
Afonin Kirill <48134029+aku536@users.noreply.github.com>
2019-11-07 17:07:56 +0300

    Добавлены файлы framework'а
    Фреймворк для работы с загрузкой фото из Flickr

---
+++ Carthage/Build/iOS/flickrAPI.framework/Headers/flickrAPI.h
@@ -0,0 +1,19 @@
+//
+//  flickrAPI.h
+//  flickrAPI
+//
+//  Created by Кирилл Афонин on 07/11/2019.
+//  Copyright © 2019 Константин Богданов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for flickrAPI.
+FOUNDATION_EXPORT double flickrAPIVersionNumber;
+
+//! Project version string for flickrAPI.
+FOUNDATION_EXPORT const unsigned char flickrAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flickrAPI/PublicHeader.h>
+
+
